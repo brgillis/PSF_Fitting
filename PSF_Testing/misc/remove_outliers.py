95ccc0e2f127fe15874a6b1f4897c735bb8f122e
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2015-09-18 13:58:41 +0100

    Upgrading remove outliers so it returns a masked array not and leaves original unaltered

--- PSF_Testing/misc/remove_outliers.py
+++ PSF_Testing/misc/remove_outliers.py
@@ -25,45 +25,49 @@
 import numpy as np
 from scipy.stats import norm
 
-def remove_outliers(olist,min_remaining_members=2):
+def remove_outliers(array,min_remaining_members=2):
     """ Removes outliers from a list of values, using Chauvenet's criterion.
     
-        Requires: olist <list of floats>
+        Requires: array <ndarray>
         Optional: min_remaining_membeters <int> (Minimum number of values that must remain. If we end
                                                  up with fewer than this, an exception will be raised.)
         
-        Returns: mean <float>, (mean of the list, after outlier removal)
-                 sigma <float> (sigma of the list, after outlier removal)
-                 
-        Side-effects: Outliers removed from olist
+        Returns: <masked array> (array with outliers masked)
         
         Except: Less than min_remaining_members values remain in olist after outlier removal                            
     """
     
-    if(min_remaining_members<2):
-        min_remaining_members = 2
+    assert(min_remaining_members>=2)
+    
+    marray = np.ma.masked_array(array) # Create a masked array from the array
     
     outliers_found = True
-    num_tot = len(olist)
+    num_tot = np.size(marray)
     
-    while(outliers_found and (len(olist)>2)):
+    while(outliers_found and (len(marray[~marray.mask])>2)):
 
         outliers_found = False
             
         # Find any outliers and remove them
         
-        mean, sigma = np.mean(olist), np.std(olist)
+        unmasked_elements = marray[~marray.mask]
         
-        for v in olist:
+        mean, sigma = np.mean(unmasked_elements), np.std(unmasked_elements)
+        
+        probs = 1-norm.cdf(abs(marray - mean) / sigma)
+        
+        outliers = probs*num_tot < 0.5
+        
+        # Do we have any new outliers?
+        if(np.any(outliers[~marray.mask])):
+            # Mark these
+            marray.mask = np.logical_or(marray.mask,outliers)
             
-            p = 1-norm.cdf(abs(v - mean) / sigma)
-            
-            if( p*num_tot < 0.5 ):
-                olist.remove(v)
-                outliers_found = True
+            # Loop through again
+            outliers_found = True
     
     # Check that we still have enough members left in the array
-    if(len(olist) < min_remaining_members):
+    if(len(marray[~marray.mask]) < min_remaining_members):
         raise Exception("WARNING: Too few arguments left after removing outliers. (Only " + str(len(list)) + ")")
     else:
-        return mean, sigma
+        return marray
