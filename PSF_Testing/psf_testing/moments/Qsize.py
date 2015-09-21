Adding test of Qsize scaling, fixing bug in default weight function

PSF_Testing/psf_testing/moments/Qsize.py
@@ -30,7 +30,7 @@ from psf_testing.moments.centre_image import centre_image
 from psf_testing.moments.coords import get_coords_of_array
 
 def get_Qsize_and_err(image,
-           weight_func = lambda x,y : 1.,
+           weight_func = lambda x,y : np.ones_like(x),
            xc = None,
            yc = None,
            x_array = None,
@@ -90,13 +90,19 @@ def get_Qsize_and_err(image,
             # If we have any pixels here, get the mean, contained mean, and variances
             I_mean[ri] = np.mean(I[ri])
             
-            for rj in xrange(ri):
-                I_lt_mean[ri] += ( I_mean[rj] * N[rj] ) / N_lt[ri]
+            if( N_lt[ri]==0 ):
+                I_lt_mean[ri] = 0
+            else:
+                for rj in xrange(ri):
+                    I_lt_mean[ri] += ( I_mean[rj] * N[rj] ) / N_lt[ri]
                 
-            var_I_mean[ri] = np.sum(I[ri])/(gain*np.square(N[ri]))
+                
+            var_I_mean[ri] = np.sum(np.abs(I[ri])/gain + np.square(background_noise)) \
+                                / (np.square(N[ri]))
                 
         # Get W for this bin
-        W[ri] = I_lt_mean[ri] - I_mean[ri]
+        if(ri>0):
+            W[ri] = I_lt_mean[ri] - I_mean[ri]
         
         # Get the covariance of this bin with itself and every smaller bin
         
@@ -139,7 +145,7 @@ def get_Qsize_and_err(image,
     # Get Qsize from this
     Qsize = Qsize_numerator/Qsize_denominator
     
-    # TODO get err_Qsize
+    # Now get the error on Qsize
     
     weighted_var_W = var_W * np.square(w_ri_array)
     

PSF_Testing/psf_testing/moments/centre_image.py
@@ -29,7 +29,7 @@ from psf_testing.moments.coords import get_x_and_y_of_array
 from psf_testing import magic_values as mv
 
 def centre_image(image,
-                 weight_func = lambda x,y: 1.,
+                 weight_func = lambda x,y: np.ones_like(x),
                  precision = 0.01):
     """ Get the centroid of an image for a given weight function.
     

PSF_Testing/psf_testing/moments/get_Qs.py
@@ -30,11 +30,11 @@ from psf_testing.moments.centre_image import centre_image
 from psf_testing.moments.estimate_background import get_background_noise
 from psf_testing.moments.get_moments import get_moments_and_variances
 from psf_testing.moments.Qsize import get_Qsize_and_err
-from psf_testing.moments.coords import get_coords_of_array
+from psf_testing.moments.coords import get_x_and_y_of_array
 from psf_testing.moments.make_weight_mask import make_weight_mask
 
 def get_m0_and_Qs(image,
-           weight_func = lambda x,y : 1.,
+           weight_func = lambda x,y : np.ones_like(x),
            xc = None,
            yc = None,
            background_noise = None,
@@ -47,7 +47,7 @@ def get_m0_and_Qs(image,
         xc, yc, x_array, y_array, weight_mask = centre_image(image, weight_func)
     else:
         nx, ny = np.shape(image)
-        x_array, y_array = get_coords_of_array(nx=nx, ny=ny, xc=xc, yc=yc)
+        x_array, y_array = get_x_and_y_of_array(nx=nx, ny=ny, xc=xc, yc=yc)
         weight_mask = make_weight_mask(weight_func=weight_func,
                                        nx=nx,
                                        ny=ny,
@@ -65,8 +65,8 @@ def get_m0_and_Qs(image,
                                                       background_noise = background_noise,
                                                       gain = gain)
     
-    ((m0,), (Mx,My), (Mxx,Myy,Mxy)) = moments_and_variances[0]
-    ((var_m0,), (var_Mx,var_My), (var_Mxx,var_Myy,var_Mxy)) = moments_and_variances[1]
+    ((m0,), (Mx,My), (_Mxx,_Myy,Mxy,Mplus)) = moments_and_variances[0]
+    ((var_m0,), (var_Mx,var_My), (_var_Mxx,_var_Myy,var_Mxy,var_Mplus)) = moments_and_variances[1]
     
     # Get the Q values from the moments, plus errors from the variances
     
@@ -84,8 +84,8 @@ def get_m0_and_Qs(image,
     var_Qy = var_My*square_scale
     err_Qy = np.sqrt(var_Qy)
     
-    Qplus = (Mxx-Myy) * square_scale
-    var_Qplus = (var_Mxx + var_Myy) * quart_scale
+    Qplus = Mplus * square_scale
+    var_Qplus = var_Mplus * quart_scale
     err_Qplus = np.sqrt(var_Qplus)
     
     Qcross = 2. * Mxy * square_scale

PSF_Testing/psf_testing/moments/get_moments.py
@@ -32,7 +32,7 @@ from psf_testing.moments.estimate_background import get_background_noise
 from psf_testing.moments.make_weight_mask import make_weight_mask
 
 def get_moments_and_variances(image,
-                weight_func = lambda x,y : 1.,
+                weight_func = lambda x,y : np.ones_like(x),
                 weight_mask = None,
                 xc = None,
                 yc = None,
@@ -62,6 +62,7 @@ def get_moments_and_variances(image,
                                        yc=yc,
                                        x_array=x_array,
                                        y_array=y_array)
+    plus_array = x2_array - y2_array
     
     # Get the unnormalized moments
     
@@ -73,6 +74,7 @@ def get_moments_and_variances(image,
     mxx = (x2_array*image*weight_mask).sum()
     myy = (y2_array*image*weight_mask).sum()
     mxy = (xy_array*image*weight_mask).sum()
+    mplus = (plus_array*image*weight_mask).sum()
     
     # Normalize the moments and store these values in a tuple of tuples
     
@@ -82,10 +84,11 @@ def get_moments_and_variances(image,
     Mxx = mxx/m0
     Myy = myy/m0
     Mxy = mxy/m0
+    Mplus = mplus/m0
     
     moments = ((m0,),
                (Mx,My),
-               (Mxx,Myy,Mxy))
+               (Mxx,Myy,Mxy,Mplus))
         
     # Now calculate the errors
     
@@ -98,6 +101,7 @@ def get_moments_and_variances(image,
     x4_array = np.square(x2_array)
     y4_array = np.square(y2_array)
     x2y2_array = np.square(xy_array)
+    plus2_array = np.square(plus_array)
     
     var_m0 = (image_var * square_weight_mask).sum()
     
@@ -107,6 +111,7 @@ def get_moments_and_variances(image,
     var_mxx = (x4_array * image_var * square_weight_mask).sum()
     var_myy = (y4_array * image_var * square_weight_mask).sum()
     var_mxy = (x2y2_array * image_var * square_weight_mask).sum()
+    var_mplus = (plus2_array * image_var * square_weight_mask).sum()
     
     # And now get the variances of the normalized moments
     square_m0 = np.square(m0)
@@ -118,9 +123,10 @@ def get_moments_and_variances(image,
     var_Mxx = var_mxx/square_m0 + np.square(mxx)*var_m0/quart_m0
     var_Myy = var_myy/square_m0 + np.square(myy)*var_m0/quart_m0
     var_Mxy = var_mxy/square_m0 + np.square(mxy)*var_m0/quart_m0
+    var_Mplus = var_mplus/square_m0 + np.square(mplus)*var_m0/quart_m0
     
     variances = ((var_m0,),
                  (var_Mx,var_My),
-                 (var_Mxx,var_Myy,var_Mxy))
+                 (var_Mxx,var_Myy,var_Mxy,var_Mplus))
     
     return moments, variances