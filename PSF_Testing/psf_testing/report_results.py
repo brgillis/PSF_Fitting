de108856f7d8c9ab35dd42b91a9853045fcfe441
brg <7204836+brgillis@users.noreply.github.com>
2015-11-05 16:18:08 +0100

    Adding reporting module and expanding test results given

---
+++ PSF_Testing/psf_testing/report_results.py
@@ -0,0 +1,24 @@
+""" @file report_results.py
+
+    Created 5 Nov 2015
+
+    Reports the results of a PSF test, printing basic results in the
+    console and detailed results in a fits table.
+
+    ---------------------------------------------------------------------
+
+    Copyright (C) 2015 Bryan R. Gillis
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
