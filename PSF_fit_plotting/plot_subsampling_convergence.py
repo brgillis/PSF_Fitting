7f95ad7995b14d34e7695b1441f3510f2d6d5f81
brg <7204836+brgillis@users.noreply.github.com>
2016-09-26 16:53:52 +0200

    Adding plots of + and x too

--- PSF_fit_plotting/plot_subsampling_convergence.py
+++ PSF_fit_plotting/plot_subsampling_convergence.py
@@ -39,7 +39,7 @@ matplotlib.rcParams['ps.useafm'] = True
 matplotlib.rcParams['pdf.use14corefonts'] = True
 matplotlib.rcParams['text.usetex'] = True
 
-
+from psf_testing.magic_values import rel_weights
 
 data_dir = "/disk2/brg/Data/HST_Fields/subsampling_convergence_testing"
 results_file_root = "jb6v09shq_sci2_cor"
@@ -55,9 +55,13 @@ def main(argv):
 
     ss_factors = np.linspace(1, 10, 10, endpoint=True).astype(int)
 
-    X2s = np.zeros_like(ss_factors,dtype=float)
-    Qs_sum_Z2s = np.zeros_like(ss_factors,dtype=float)
-    Qs_diff_Z2s = np.zeros_like(ss_factors,dtype=float)
+    X2s = np.zeros_like(ss_factors, dtype=float)
+    Qp_sum_Z2s = np.zeros_like(ss_factors, dtype=float)
+    Qp_diff_Z2s = np.zeros_like(ss_factors, dtype=float)
+    Qc_sum_Z2s = np.zeros_like(ss_factors, dtype=float)
+    Qc_diff_Z2s = np.zeros_like(ss_factors, dtype=float)
+    Qs_sum_Z2s = np.zeros_like(ss_factors, dtype=float)
+    Qs_diff_Z2s = np.zeros_like(ss_factors, dtype=float)
 
     for ssf in ss_factors:
 
@@ -65,9 +69,13 @@ def main(argv):
 
         header = fits.open(filename)[1].header
 
-        X2s[ssf-1] = header["X_SQR"]
-        Qs_sum_Z2s[ssf-1] = header["QSS_Z2"]
-        Qs_diff_Z2s[ssf-1] = header["QSD_Z2"]
+        X2s[ssf - 1] = header["X_SQR"]
+        Qp_sum_Z2s[ssf - 1] = header["QPS_Z2"] / np.square(rel_weights["Qpcs_diff"][0])
+        Qp_diff_Z2s[ssf - 1] = header["QPD_Z2"] / np.square(rel_weights["Qpcs_diff"][0])
+        Qc_sum_Z2s[ssf - 1] = header["QPS_Z2"] / np.square(rel_weights["Qpcs_diff"][1])
+        Qc_diff_Z2s[ssf - 1] = header["QPD_Z2"] / np.square(rel_weights["Qpcs_diff"][1])
+        Qs_sum_Z2s[ssf - 1] = header["QSS_Z2"] / np.square(rel_weights["Qpcs_diff"][2])
+        Qs_diff_Z2s[ssf - 1] = header["QSD_Z2"] / np.square(rel_weights["Qpcs_diff"][2])
 
     # Plot it up
     _fig = pyplot.figure(figsize=figsize)
@@ -78,12 +86,16 @@ def main(argv):
     ax = pyplot.subplot(gs[0])
 
     ax.plot(ss_factors, X2s, label="X2")
-    ax.plot(ss_factors, Qs_sum_Z2s/100, label="Qs sum Z2")
-    ax.plot(ss_factors, Qs_diff_Z2s/100, label="Qs diff Z2")
-    
-    ax.set_xlim([0.5,10.5])
-    ax.set_ylim([0,0.002])
-    
+    ax.plot(ss_factors, Qp_sum_Z2s, label="Qp sum Z2")
+    ax.plot(ss_factors, Qp_diff_Z2s, label="Qp diff Z2")
+    ax.plot(ss_factors, Qc_sum_Z2s, label="Qc sum Z2")
+    ax.plot(ss_factors, Qc_diff_Z2s, label="Qc diff Z2")
+    ax.plot(ss_factors, Qs_sum_Z2s, label="Qs sum Z2")
+    ax.plot(ss_factors, Qs_diff_Z2s, label="Qs diff Z2")
+
+    ax.set_xlim([0.5, 10.5])
+    ax.set_ylim([0, 0.002])
+
     ax.legend(loc="upper right")
 
     pyplot.show()
