e433cf6b8b0f477999c696ccf3a8e4c112dbaad0
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2016-10-13 13:55:05 +0100

    Adding options to make generation of multiple fields easier.

--- PSF_Testing/make_control_field.py
+++ PSF_Testing/make_control_field.py
@@ -27,8 +27,6 @@
 import sys
 from psf_testing.control_field import make_control_field
 
-num_fields = 100
-
 def main(argv):
     """ @TODO main docstring
     """
@@ -38,30 +36,76 @@ def main(argv):
         debugging = True
     except ImportError:
         debugging = False
-        
-    for i in range(num_fields):
-        
-        focus = -6.0 + (10.0*i)/num_fields
-        
-        make_control_field("/disk2/brg/Data/HST_Fields/control_image_"+str(i)+".fits",
-                   random_seed=10*(i+1),
-                   num_grid_points=(0, 0),
-                   focus=focus,
-                   sky_level=0.,
-                   read_noise=51., # Approximately right for background, but doesn't include unresolved sources
-                   # base_image="/disk2/brg/Data/HST_Fields/mock_galaxies_"+str(i)+".fits",
-                   suppress_noise=False,
-                   num_stars=1000,
-                   binary_fraction=0.0,
-                   binary_r_max=1.,
-                   subsampling_factor=10,
-                   guiding_error_mag1=0.000/0.05,
-                   guiding_error_mag2=0.000/0.05,
-                   guiding_error_angle=0.,
-                   randomize_spectral_type=False,
-                   use_cache=True,
-                   parallelize=not debugging)
-    pass
+
+	num_fields = 100
+	i_start = 0
+	mode = "none"
+
+	if len(argv)>1:
+		num_fields = int(argv[1])
+	if len(argv)>2:
+		i_start = int(argv[2])
+
+	if len(argv)>3:
+		mode = argv[3]
+
+	tag = ""
+	base_image_root = None
+	binary_fraction = 0.0
+	binary_r_max = 1.
+	guiding_error_mag1=0.
+	guiding_error_mag2=0.
+	randomize_spectral_type=False
+
+	if mode!="none":
+		tag = "_" + mode
+
+	if mode == "gb" or mode == "full":
+		base_image_root = "/home/brg/Data/HST_Fields/mock_galaxies_"
+	if mode == "b3" or mode == "full":
+		binary_fraction = 0.3
+	if mode == "b32":
+		binary_fraction = 0.3
+		binary_r_max = 2.
+	if mode == "blur":
+		guiding_error_mag1 = 0.010/0.005
+	if mode == "blur2" or mode == "full":
+		guiding_error_mag1 = 0.010/0.005
+		guiding_error_mag2 = 0.010/0.005
+	if mode == "rs" or mode == "full":
+		randomize_spectral_type = True
+		
+
+
+	def run_for_i(i):
+		focus = -6.0 + (10.0*(i%100))/100
+
+		if base_image_root is not None:
+			base_image=base_image_root+str(i)+".fits"
+		else:
+			base_image = None
+
+		make_control_field("/home/brg/Data/HST_Fields/control_image_"+str(i)+"_blur2.fits",
+				   random_seed=10*(i+1),
+				   num_grid_points=(0, 0),
+				   focus=focus,
+				   sky_level=0.,
+				   read_noise=51., # Approximately right for background, but doesn't include unresolved sources
+				   base_image=base_image,
+				   suppress_noise=False,
+				   num_stars=1000,
+				   binary_fraction=binary_fraction,
+				   binary_r_max=binary_r_max,
+				   subsampling_factor=10,
+				   guiding_error_mag1=guiding_error_mag1,
+				   guiding_error_mag2=guiding_error_mag2,
+				   guiding_error_angle=0.,
+				   randomize_spectral_type=randomize_spectral_type,
+				   use_cache=True,
+				   parallelize=True)
+		
+	for i in range(num_fields):
+		run_for_i(i+i_start)
 
 if __name__ == "__main__":
     main(sys.argv)
